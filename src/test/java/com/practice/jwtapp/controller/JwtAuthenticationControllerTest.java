4a565e803441b4d8a6d26cd3da7d799f51df965f
UsagiDrops <kristofferkramer83@gmail.com>
2020-12-04 15:24:21 +0100

    createAuthenticationToken controller test

--- src/test/java/com/practice/jwtapp/controller/JwtAuthenticationControllerTest.java
+++ src/test/java/com/practice/jwtapp/controller/JwtAuthenticationControllerTest.java
@@ -3,30 +3,46 @@ package com.practice.jwtapp.controller;
 import com.practice.jwtapp.config.JwtTokenUtil;
 import com.practice.jwtapp.model.JwtRequest;
 import com.practice.jwtapp.model.JwtResponse;
+import com.practice.jwtapp.service.JwtUserDetailsService;
+import com.practice.jwtapp.service.UserServiceImpl;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.User;
+
+import java.util.Collections;
+
+import static org.mockito.Mockito.times;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class JwtAuthenticationControllerTest {
-
+    @InjectMocks
     private JwtAuthenticationController jwtAuthenticationController;
+
     private JwtRequest jwtRequest;
-    private JwtTokenUtil jwtTokenUtil;
+
     private String token;
 
     @Mock
     AuthenticationManager authenticationManager;
 
+    @Mock
+    JwtUserDetailsService userDetailsService;
+
+    @Mock
+    JwtTokenUtil jwtTokenUtil;
+
     @BeforeAll
     public void setup() {
+
         jwtAuthenticationController = new JwtAuthenticationController();
         jwtRequest = new JwtRequest("password", "defaultUser");
         MockitoAnnotations.initMocks(this);
@@ -34,10 +50,21 @@ public class JwtAuthenticationControllerTest {
 
     @Test
     public void createAuthenticationToken() throws Exception {
-        Mockito.when(authenticationManager.authenticate(
-                new UsernamePasswordAuthenticationToken(jwtRequest.getUsername(), jwtRequest.getPassword())));
+        Mockito.when(userDetailsService.loadUserByUsername(jwtRequest.getUsername()))
+                .thenReturn(createUser(jwtRequest));
 
         ResponseEntity<?> responseEntity = jwtAuthenticationController.createAuthenticationToken(jwtRequest);
-        Assertions.assertEquals(responseEntity,  ResponseEntity.ok(new JwtResponse(token)));
+        Assertions.assertEquals(ResponseEntity.ok(new JwtResponse(token)).getStatusCode(), responseEntity.getStatusCode());
+
+        Mockito.verify(authenticationManager, times(1))
+                .authenticate(new UsernamePasswordAuthenticationToken(jwtRequest.getUsername(), jwtRequest.getPassword()));
+        Mockito.verify(userDetailsService, times(1))
+                .loadUserByUsername(jwtRequest.getUsername());
+        Mockito.verify(jwtTokenUtil,times(1))
+                .generateToken(createUser(jwtRequest));
+    }
+
+    private User createUser(JwtRequest jwtRequest) {
+        return new User(jwtRequest.getUsername(), jwtRequest.getPassword(), Collections.emptySet());
     }
 }
